18415ce501f094f1b3f3b9907860c17c36e4f8eb
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-04-30 14:24:01 +0300

    Initial Commit

---
+++ Contacts HW2.7 Gnilitskiy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts HW2.7 Gnilitskiy
+//
+//  Created by Александр on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
